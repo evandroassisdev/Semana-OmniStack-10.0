8b634aa8f4b3bb116e48a508c1308979b0a89a9d
Evandro <evandroassisdev@gmail.com>
2020-01-16 14:43:06 -0400

    fix: backend & web finished

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
-import './global.css'
-import './App.css'
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+    console.log(response);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
-    <div id="app">
+    <div id='app'>
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <label htmlFor="">Usuário do Github</label>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
-      <main></main>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
